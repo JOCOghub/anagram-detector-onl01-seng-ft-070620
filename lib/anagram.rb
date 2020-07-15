905b2a6ba2941b29e3a6c82fc8ef2180dd843a40
Joseph cohen <jojocohen57@gmail.com>
2020-07-15 23:05:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word 
+  
+  def initialize word
+    @word = word 
+  end
+  
+  def match(compare_words)
+    compare_words.select {|compare_word| compare_word.split("").sort == word.split("").sort}
+  end  
+
+end   
