35450b11e86b92b9aee80cc2f8c88cb21fc167cd
Joseph cohen <jojocohen57@gmail.com>
2020-07-15 23:04:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
 # Your code goes here!
+require 'pry'
 class Anagram
   attr_accessor :word 
   
@@ -8,6 +9,7 @@ class Anagram
   
   def match(compare_words)
     compare_words.select {|compare_word| compare_word.sort == word.sort}
+    binding.pry
   end  
 
 end   
